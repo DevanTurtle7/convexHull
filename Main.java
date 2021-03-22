22b39bec6e5f753da285d9a95f6f719de63c5938
Devan Kavalchek <devankav7@gmail.com>
2021-03-22 17:00:35 -0400

    added cli

--- Main.java
+++ Main.java
@@ -1,22 +1,40 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Enter coordinates in the format <x> <y>");
+        System.out.println("Press enter to solve the convex hull.");
+
         List<Point> points = new ArrayList<>();
-        points.add(new Point(0, 2));
-        points.add(new Point(-1, 2));
-        points.add(new Point(4, 2));
-        points.add(new Point(6, 3));
-        points.add(new Point(2, -3));
-        points.add(new Point(-1, -1));
-        points.add(new Point(2, 0));
-        points.add(new Point(3, 6));
-        points.add(new Point(5, -1));
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+
+        while (input != "") {
+            String[] tokens = input.split(" ");
+
+            try {
+                int x = Integer.parseInt(tokens[0]);
+                int y = Integer.parseInt(tokens[1]);
+                Point point = new Point(x, y);
+                points.add(point);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, try again.");
+            }
+
+            input = scanner.nextLine();
+        }
+
+        scanner.close();
 
         Collections.sort(points, new PointComparator());
+        List<Point> path = Solver.convexHull(points, points.get(0));
 
-        Solver.convexHull(points, points.get(0));
+        for (Point point : path) {
+            System.out.println(point);
+        }
     }
 }
